2393aefcc5c90e43d70d70f38bc28e8765bad110
Wesley Tuschi <levituschi@gmail.com>
2023-10-10 22:42:45 -0300

    feat: add projects responsive

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import '../styles/Portfolio.css';
 import { motion } from 'framer-motion';
 import RecipesApp from '../images/logoRecipes.png';
+import TriviaTime from '../images/triviaTime.jpg';
 import { ReactComponent as Web } from '../assets/web-svgrepo-com.svg';
 import { ReactComponent as GitHub } from '../assets/github-142-svgrepo-com.svg';
 
@@ -64,6 +65,38 @@ const Portfólio = () => {
               O objetivo do aplicativo é auxiliar todos aqueles que amam
               cozinhar, mas às vezes ficam perdidos sobre como preparar seus
               pratos favoritos.
+            </p>
+            <div className='project-links'>
+              <a
+                href='https://recipes-app-two-silk.vercel.app/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <Web className='web-logo img-fluid' />
+              </a>
+              <a
+                href='https://github.com/wesley-tuschi/recipes-app'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <GitHub className='github-logo img-fluid' />
+              </a>
+            </div>
+          </div>
+          </motion.div>
+          <motion.div
+          className='card'
+          variants={cardVariants}
+          initial='hidden'
+          animate={isVisible ? 'visible' : 'hidden'}
+        >
+          <img src={TriviaTime} className='card-img-top img-fluid' alt='Trivia Time'/>
+          <div className='card-body'>
+            <h5 className='card-title'>
+              <b>Trivia</b>
+            </h5>
+            <p className='card-text'>
+              O objetivo do aplicativo é treinar o conhecimento com o jogo Trivia.
             </p>
             <div className='project-links'>
               <a
