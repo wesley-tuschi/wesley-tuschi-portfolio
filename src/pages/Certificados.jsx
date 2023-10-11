24697260bc73018c5bad64c22d249b3d0d4a93ca
Wesley Tuschi <levituschi@gmail.com>
2023-10-11 13:11:56 -0300

    feat: certificados responsive

--- src/pages/Certificados.jsx
+++ src/pages/Certificados.jsx
@@ -24,70 +24,70 @@ const Certificados = () => {
           <div className='carousel-item active'>
             <img
               src={BasicFrontEnd}
-              className='d-block w-100'
+              className='d-block w-100 img-certificados'
               alt='Certificado de Front-end Básico'
             ></img>
           </div>
           <div className='carousel-item'>
             <img
               src='https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/69606710'
-              className='d-block w-100'
+              className='d-block w-100 img-certificados'
               alt='Certificado 2'
             ></img>
           </div>
           <div className='carousel-item'>
             <img
               src='https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/75610904'
-              className='d-block w-100'
+              className='d-block w-100 img-certificados'
               alt='Certificado 3'
             ></img>
           </div>
           <div className='carousel-item'>
             <img
               src={HtmlCss1}
-              className='d-block w-100'
+              className='d-block w-100 img-certificados'
               alt='Certificado de HTML e CSS 1'
             ></img>
           </div>
           <div className='carousel-item'>
             <img
               src={HtmlCss2}
-              className='d-block w-100'
+              className='d-block w-100 img-certificados'
               alt='Certificado de HTML e CSS 2'
             ></img>
           </div>
           <div className='carousel-item'>
             <img
               src={HttpCurso}
-              className='d-block w-100'
+              className='d-block w-100 img-certificados'
               alt='Certificado de Curso HTTP'
             ></img>
           </div>
           <div className='carousel-item'>
             <img
               src={Liderança}
-              className='d-block w-100'
+              className='d-block w-100 img-certificados'
               alt='Certificado de Liderança'
             ></img>
           </div>
           <div className='carousel-item'>
             <img
               src={Pentest}
-              className='d-block w-100'
+              className='d-block w-100 img-certificados'
               alt='Certificado de Pentest'
             ></img>
           </div>
           <div className='carousel-item'>
             <img
               src={ReactCurso}
-              className='d-block w-100'
+              className='d-block w-100 img-certificados'
               alt='Certificado de Curso React'
             ></img>
           </div>
           <div className='carousel-item'>
             <img
               src={SegurancaRede}
-              className='d-block w-100'
+              className='d-block w-100 img-certificados'
               alt='Certificado de Segurança de Rede'
             ></img>
           </div>
