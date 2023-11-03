44f9a57d4117290c0d8424c793b86c9738e28562
Wesley Tuschi <levituschi@gmail.com>
2023-11-03 17:11:50 -0300

    feat: add project TAD

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,6 +3,7 @@ import '../styles/Portfolio.css';
 import RecipesApp from '../images/logoRecipes.png';
 import TriviaTime from '../images/triviaTime.jpg';
 import TrybeTunes from '../images/trybeTunes.png';
+import TAD from '../images/TAD.png';
 import { ReactComponent as Web } from '../assets/web-svgrepo-com.svg';
 import { ReactComponent as GitHub } from '../assets/github-142-svgrepo-com.svg';
 
@@ -103,6 +104,32 @@ const Portfolio = () => {
                 </a>
                 <a
                   href='https://github.com/wesley-tuschi/project-trybetunes'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <GitHub className='github-logo img-fluid' />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className='card'>
+            <img
+              src={TAD}
+              className='card-img-top img-fluid'
+              alt='Trivia Time'
+            />
+            <div className='card-body'>
+              <h5 className='card-title'>
+                <b>Trybers And Dragons (Back-End)</b>
+              </h5>
+              <p className='card-text'>
+                O objetivo do projeto foi implementar com TypeScript seguindo os
+                principios da arquitetura SOLID e da Programação Orientada a
+                Objetos (POO).
+              </p>
+              <div className='project-links justify-content-center'>
+                <a
+                  href='https://github.com/wesley-tuschi/project-trybers-and-dragons'
                   target='_blank'
                   rel='noopener noreferrer'
                 >
